0bb52fa98d9429c92bc7cf0f22cda7213515cd74
308 <308@academicos.cesde.edu.co>
2022-02-22 20:39:38 -0500

    Finalizacion del juego DOMJS

--- funciones.js
+++ funciones.js
@@ -52,6 +52,15 @@ document.addEventListener("DOMContentLoaded", function(){
     ]
     //tablero HTML
     let tablero = document.querySelector(".tablero");
+    //Arreglo para guardar el nombre de las imagenes
+    let imgElegida = [];
+    //Arreglo para guardar el ID de las imagenes
+    let imgElegidaID = [];
+    let aciertos = document.querySelector(".conteo");
+    let conteo = [];
+    
+    //Funcion para colocar las imagenes aleatorias
+    imagenes.sort(()=>0.5 - Math.random());
     //Funcion para colocar las imagenes en el HTML
     function crearTablero(){
         for(let i=0; i< imagenes.length; i++){
@@ -60,7 +69,45 @@ document.addEventListener("DOMContentLoaded", function(){
             img.setAttribute("src","imagenes/Pregunta.jpg");
             img.setAttribute("width","200px");
             tablero.appendChild(img);
+            img.addEventListener("click",descubrirImagen);
         }
+    }
+    //Descubrir las imagenes
+    function descubrirImagen(){
+      let imgClick = this.getAttribute("data-id");
+      imgElegida.push(imagenes[imgClick].name);
+      imgElegidaID.push(imgClick);
+      this.setAttribute("src",imagenes[imgClick].img);
+      
+      if(imgElegida.length === 2){
+        setTimeout(compararImagen, 300);
+      }
+    }
+
+    
+    //Funcion para comparar las imagenes
+    function compararImagen(){
+      let todasLasImg = document.querySelectorAll("img");
+      let opcion1 = imgElegidaID[0];
+      let opcion2 = imgElegidaID[1];
+      if(imgElegida[0] === imgElegida[1]){
+        alert("Acertaste");
+        todasLasImg[opcion1].setAttribute("src","imagenes/aciertos.jpg");
+        todasLasImg[opcion2].setAttribute("src","imagenes/aciertos.jpg");
+        conteo.push(imgElegida);
+      }else{
+        alert("Sigue Intentando");
+        todasLasImg[opcion1].setAttribute("src","imagenes/Pregunta.jpg");
+        todasLasImg[opcion2].setAttribute("src","imagenes/Pregunta.jpg");
+      }
+      //Volver a llenar el arreglo
+      imgElegida = [];
+      imgElegidaID = [];
+      aciertos.textContent = conteo.length;
+
+      if(conteo.length === 6){
+        aciertos.textContent = "Ganaste";
+      }
     }
 
     crearTablero();
